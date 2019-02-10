1a31a66474bdb388264511b750e61ea6bbe77d87
PlayMa256 <matheus.g.silva@hotmail.com>
2019-02-10 18:29:25 -0200

    chore: finally converted everything to a class, added eslint, started writing the beggining of CLI

--- index.js
+++ index.js
@@ -1,107 +1,23 @@
-const { exec } = require('child_process');
-const ytlist = require('youtube-playlist');
-const youtubedl = require('youtube-dl');
 const path = require('path');
-const fs = require('fs');
-const fetchVideoInfo = require('youtube-info');
-const sanitize = require("sanitize-filename");
 
+const chalk = require('chalk');
+const parse = require('yargs-parser');
 
-class PlaylistDownload {
-    constructor(options){
-        this.options = options;
-    }
+const pkg = require('./package.json');
 
-    parseMp3(videoFilename, outputFolder){
-        const regex = /(\.mp4)/g;
-        const MP3fileName = videoFilename.replace(regex, '.mp3');
-    
-        const mp4 = outputFolder + "/mp4/" + videoFilename;
-        const mp3 = outputFolder + MP3fileName;
-    
-        return new Promise((resolve, reject) => {
-            exec('ffmpeg -i "' + mp4 + '" "' + mp3 + '"', (error) => {
-                    // app.deleteVideo(fileName, callback);
-                    if (error !== null) {
-                        reject(error);
-                    }
-                    resolve();
-                }
-            );
-        })
-    }
+const help = chalk`
+  ${pkg.description}
+  {underline Usage}
+    $ yt-pl-download <playlist URL> <output folder>
 
-    async fetchPlaylistVideos(){
-        const response = await ytlist(this.options.url, 'url');
-        return response.data.playlist || [];
-    }
+  {underline Options}
+    --help            Displays this message
 
-    fetchVideoInfo(video){
-        const videoId = video.split('v=')[1];
-        
-        return new Promise((resolve, reject) => {
-            fetchVideoInfo(videoId, (err, videoInfo) => {
-                if(err){
-                    reject(err);
-                }
-                resolve(videoInfo);
-            });
-        })
-    }
+  {underline Examples}
+    $ yt-pl-download --help
+    $ yt-pl-download v016 src
+`;
 
-    async run() {
-        const playlistVideos = await this.fetchPlaylistVideos();
-        playlistVideos.forEach((video) => {
-            const videoInfo = this.fetchVideoInfo(video);
-        })
-    }
-}
-
-
-ytlist('https://www.youtube.com/playlist?list=PLGEL0vaf2SSFu12LIEyRd2ya29yGllnGe', 'url').then(res => {
-    const videos = res.data.playlist;
-    videos.forEach((video) => {
-        const videoId = video.split('v=')[1];
-        fetchVideoInfo(videoId, (err, videoInfo) => {
-            const title = videoInfo.title;
-
-            const videoDownload = youtubedl(video);
-            var size = 0;
-            videoDownload.on('error', function(err) {
-                console.log("Error 2:  " + err);
-                callback();
-            });
-
-            videoDownload.on('info', function(info) {
-                size = info.size;
-                videoDownload.pipe(fs.createWriteStream(path.resolve(__dirname, 'mp4', sanitize(`${title}.mp4`))));
-            });
-
-            var pos = 0;
-            videoDownload.on('data', function(data) {
-                pos += data.length;
-                if (size) {
-                    var percent = (pos / size * 100).toFixed(2);
-                    if (percent == 100) {
-                        return 0;
-                    }
-                    else {
-                        process.stdout.cursorTo(0);
-                        process.stdout.clearLine(1);
-                        process.stdout.write(percent + '%');
-                    }
-                }
-            });
-
-            videoDownload.on('end', function() {
-                process.stdout.cursorTo(0);
-                process.stdout.clearLine(1);
-                process.stdout.write('---Complete \n');
-
-                
-            });
-        });
-    })
-});
-
-
+const run = async () => {
+  const argv = parse(process.argv.slice(2));
+};
---
+++ playlistDownloader.js
@@ -0,0 +1,169 @@
+const { exec } = require('child_process');
+
+const path = require('path');
+
+const fs = require('fs');
+
+const ytlist = require('youtube-playlist');
+const youtubedl = require('youtube-dl');
+
+const youtubeFetchVideoInfo = require('youtube-info');
+const sanitize = require('sanitize-filename');
+
+class PlaylistDownload {
+  constructor(options) {
+    this.options = {
+      ...options,
+      mp4FolderPath: path.resolve(options.outputFolder, 'mp4')
+    };
+  }
+
+  async fetchPlaylistVideos() {
+    const response = await ytlist(this.options.url, 'url');
+    return response.data.playlist || [];
+  }
+
+  deleteVideo(videoFilename) {
+    fs.unlinkSync(path.resolve(this.options.mp4FolderPath, videoFilename));
+  }
+
+  parseMp3(videoFilename) {
+    const regex = /(\.mp4)/g;
+    const MP3fileName = videoFilename.replace(regex, '.mp3');
+
+    const mp4 = `${this.options.outputFolder}/mp4/${videoFilename}`;
+    const mp3 = this.options.outputFolder + MP3fileName;
+
+    return new Promise((resolve, reject) => {
+      exec(`ffmpeg -i "${mp4}" "${mp3}"`, (error) => {
+        if (error !== null) {
+          reject(error);
+        }
+        this.deleteVideo(videoFilename);
+        process.stdout.write('-- Mp3 converted successfully -- \n');
+        resolve();
+      });
+    });
+  }
+
+  // eslint-disable-next-line
+  fetchVidInfo(video) {
+    // eslint-disable-next-line
+    const [_, videoId] = video.split('v=');
+
+    return new Promise((resolve, reject) => {
+      youtubeFetchVideoInfo(videoId, (err, videoInfo) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(videoInfo);
+      });
+    });
+  }
+
+  downloadVideo(video, videoInfo) {
+    const videoFolder = this.options.mp4FolderPath;
+    if (!fs.existsSync(videoFolder)) {
+      fs.mkdirSync(videoFolder);
+    }
+
+    return new Promise((resolve, reject) => {
+      const title = sanitize(videoInfo.title);
+      const mp4TitleWithExtension = `${title}.mp4`;
+      const videoDownload = youtubedl(video);
+      let size = 0;
+      videoDownload.on('error', (err) => {
+        console.log(`Error 2:  ${err}`);
+        reject(err);
+      });
+
+      videoDownload.on('info', ({ size: downloadSize }) => {
+        size = downloadSize;
+        videoDownload.pipe(fs.createWriteStream(path.resolve(videoFolder, mp4TitleWithExtension)));
+        process.stdout.write(`Downloading "${title}"`);
+      });
+
+      let pos = 0;
+      // eslint-disable-next-line
+      videoDownload.on('data', (data) => {
+        pos += data.length;
+        if (size) {
+          const percent = ((pos / size) * 100).toFixed(2);
+          if (percent === 100) {
+            return 0;
+          }
+          process.stdout.cursorTo(0);
+          process.stdout.clearLine(1);
+          process.stdout.write(`${percent}%`);
+        }
+      });
+
+      videoDownload.on('end', async () => {
+        process.stdout.cursorTo(0);
+        process.stdout.clearLine(1);
+        process.stdout.write(`-- ${title} Downloaded successfully -- \n`);
+        process.stdout.write(`-- Converting ${title} to mp3 -- \n`);
+        await this.parseMp3(mp4TitleWithExtension);
+        resolve();
+      });
+    });
+  }
+
+  async run() {
+    if (!fs.existsSync(path.resolve(this.options.outputFolder))) {
+      fs.mkdirSync(path.resolve(this.options.outputFolder));
+    }
+
+    const playlistVideos = await this.fetchPlaylistVideos();
+
+    playlistVideos.forEach(async (video) => {
+      const videoInfo = this.fetchVidInfo(video);
+      await this.downloadVideo(video, videoInfo);
+    });
+  }
+}
+
+// ytlist('https://www.youtube.com/playlist?list=PLGEL0vaf2SSFu12LIEyRd2ya29yGllnGe', 'url').then((res) => {
+//   const videos = res.data.playlist;
+//   videos.forEach((video) => {
+//     const videoId = video.split('v=')[1];
+//     youtubeFetchVideoInfo(videoId, (err, videoInfo) => {
+//       const title = videoInfo.title;
+
+//       const videoDownload = youtubedl(video);
+//       let size = 0;
+//       videoDownload.on('error', (err) => {
+//         console.log(`Error 2:  ${err}`);
+//         callback();
+//       });
+
+//       videoDownload.on('info', (info) => {
+//         size = info.size;
+//         videoDownload.pipe(fs.createWriteStream(path.resolve(__dirname, 'mp4', sanitize(`${title}.mp4`))));
+//       });
+
+//       let pos = 0;
+//       videoDownload.on('data', (data) => {
+//         pos += data.length;
+//         if (size) {
+//           const percent = (pos / size * 100).toFixed(2);
+//           if (percent == 100) {
+//             return 0;
+//           }
+
+//           process.stdout.cursorTo(0);
+//           process.stdout.clearLine(1);
+//           process.stdout.write(`${percent}%`);
+//         }
+//       });
+
+//       videoDownload.on('end', () => {
+//         process.stdout.cursorTo(0);
+//         process.stdout.clearLine(1);
+//         process.stdout.write('---Complete \n');
+//       });
+//     });
+//   });
+// });
+
+module.exports = { PlaylistDownload };
